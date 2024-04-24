5259d075508a5288140cab87e106ada3eb652e96
Danilo Adrian <daniloxct@gmail.com>
2024-04-24 16:23:12 -0300

    firts commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            CEP: {
+                required: true,
+            },
+            CPF: {
+                required: true,
+            },
+            endereco: {
+                required: true,
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira seu nome'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids()
+            alert(`existem ${camposIncorretos} campos incorretos`)
+        }
+    })
+})
