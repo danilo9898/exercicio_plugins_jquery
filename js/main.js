a8985e4da18a8a835451c641c7bfdea0c4c8da9a
Danilo Adrian <daniloxct@gmail.com>
2024-04-26 13:42:08 -0300

    atualizações

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
     $('#telefone').mask('(00) 00000-0000')
+    $('#CEP').mask('00000-000')
+    $('#CPF').mask('000.000.000.00')
 
     $('form').validate({
         rules: {
